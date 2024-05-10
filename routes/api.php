53968052c4700f3de9ec63279418ebd2b9b8f72f
Alfian Ramadani <Alfianr792@gmail.com>
2024-05-10 20:28:19 +0800

    feat : adding new routes API

--- routes/api.php
+++ routes/api.php
@@ -1,15 +1,19 @@
 <?php
 
-use App\Http\Controllers\AccountController;
+use App\Http\Controllers\EducationJurneyController;
+use App\Http\Controllers\ExprerienceJourneyController;
+use App\Http\Controllers\informationController;
+use App\Http\Controllers\ProjectsController;
+use App\Http\Controllers\SocialMediaController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::get('account',[AccountController::class,'index']); 
-Route::get('account/{id}',[AccountController::class,'show']); 
-Route::post('account',[AccountController::class,'store']); 
-Route::put('account/{id}',[AccountController::class,'update']); 
-Route::delete('account/{id}',[AccountController::class,'destroy']); 
 
+Route::get('/information', [informationController::class, 'index']);
+Route::get('/education', [EducationJurneyController::class, 'index']);
+Route::get('/experience', [ExprerienceJourneyController::class, 'index']);
+Route::get('/socialmedia', [SocialMediaController::class, 'index']);
+Route::get('/projects/{id}', [informationController::class, 'show']);
