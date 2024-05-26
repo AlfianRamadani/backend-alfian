8302443febffb1a34de4b43cef76afeb5ee9bea2
Alfian Ramadani <Alfianr792@gmail.com>
2024-05-26 10:38:50 +0800

    chore: idk

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,13 @@
 <?php
 
+use App\Helpers\ResponseFormatter;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return ['Laravel' => app()->version()];
-});
+    Route::get('/', function () {
+        return "hello";
+    });
 
-require __DIR__.'/auth.php';
+    require __DIR__.'/auth.php';
+// Route::fallback(function () {
+//     return ResponseFormatter::error(NULL, " Not Valid Route", 422);
+// });
