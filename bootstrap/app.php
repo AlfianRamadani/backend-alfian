1c3f43f0eac77238cfc95caf67331cdeea1fcb5b
Alfian Ramadani <Alfianr792@gmail.com>
2024-05-26 10:38:15 +0800

    feat : add custom handling error

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,35 +1,33 @@
 <?php
 
+use App\Helpers\ResponseFormatter;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
-use Illuminate\Auth\AuthenticationException;
-use Illuminate\Http\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\Routing\Exception\MethodNotAllowedException;
 
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',
-        commands: __DIR__.'/../routes/console.php',
+        api: __DIR__ . '/../routes/api.php',
+
+        web: __DIR__ . '/../routes/web.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->api(prepend: [
-            \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
-        ]);
-
-        $middleware->alias([
-            'verified' => \App\Http\Middleware\EnsureEmailIsVerified::class,
-        ]);
-
         //
     })
     ->withExceptions(function (Exceptions $exceptions) {
-            $exceptions->render(function (AuthenticationException $e, Request $request) {
-                if ($request->is('api/*')) {
-                    return response()->json([
-                        'message' => $e->getMessage(),
-                    ], 401);
-                }
-            });    })->create();
+        $exceptions->render(function (NotFoundHttpException $e) {
+            return response()->json([
+                'message' => 'Not Foundd',
+                'status' => 404
+
+            ]);
+        });
+        $exceptions->render(function (MethodNotAllowedException $e) {
+            return ResponseFormatter::error(NULL, "Method Not Allowed", 405);
+        });
+    })->create();
