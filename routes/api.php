e0190b4d21d538bf8960ec8550c7233fd76e30c0
Alfian Ramadani <Alfianr792@gmail.com>
2024-05-26 10:37:14 +0800

    feat : add api routes

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,75 @@
 <?php
 
-use App\Http\Controllers\EducationJurneyController;
-use App\Http\Controllers\ExprerienceJourneyController;
+use App\Helpers\ResponseFormatter;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\EducationController;
+use App\Http\Controllers\ExprerienceController;
+use App\Http\Controllers\FormController;
 use App\Http\Controllers\informationController;
+use App\Http\Controllers\JourneyController;
+use App\Http\Controllers\PostController;
 use App\Http\Controllers\ProjectsController;
 use App\Http\Controllers\SocialMediaController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+// Route::get('/information', [informationController::class, 'index']);
+// Route::get('/education', [EducationJurneyController::class, 'index']);
+// Route::get('/experience', [ExprerienceJourneyController::class, 'index']);
+// Route::get('/socialmedia', [SocialMediaController::class, 'index']);
+// Route::get('/projects/{id}', [informationController::class, 'show']);
+// Route::post('/projects', [informationController::class, "store"]);
+Route::fallback(function () {
+    return ResponseFormatter::error(NULL, " Not Valid Route");
 });
 
-Route::get('/information', [informationController::class, 'index']);
-Route::get('/education', [EducationJurneyController::class, 'index']);
-Route::get('/experience', [ExprerienceJourneyController::class, 'index']);
-Route::get('/socialmedia', [SocialMediaController::class, 'index']);
-Route::get('/projects/{id}', [informationController::class, 'show']);
+Route::controller(informationController::class)->group(function () {
+    Route::get('/information', 'index');
+    Route::get('/information/{id}', 'show');
+    Route::post('/information', 'store');
+    Route::put('/information/{id}', 'update');
+    Route::delete('/information/{id}', 'destroy');
+});
+
+Route::controller(ProjectsController::class)->group(function () {
+    Route::get('/projects', 'index');
+    Route::get('/projects/{id}', 'show');
+    Route::post('/projects', 'store');
+    Route::put('/projects/{id}', 'update');
+    Route::delete('/projects/{id}', 'destroy');
+});
+
+Route::controller(PostController::class)->group(function () {
+    Route::get('/post', 'index');
+    Route::post('/post', 'store');
+    Route::put('/post/{id}', 'update');
+    Route::delete('/post/{id}', 'destroy');
+    Route::get('/post/{id}', 'postBasedId');
+});
+
+
+Route::controller(EducationController::class)->group(function () {
+    Route::get('/education', 'index');
+    Route::put('education/{id}', 'update');
+
+});
+
+Route::controller(FormController::class)->group(function () {
+    Route::post('/form', 'store');
+    Route::get('/form', 'index');
+});
+
+Route::controller(JourneyController::class)->group(function () {
+    Route::get('/journey', 'index');
+    Route::put('/journey/{id}', 'update');
+});
+Route::controller(CategoryController::class)->group(function () {
+    Route::get('/category', 'index');
+    // Route::put('/category/{id}', 'update');
+    Route::get("/category/{category}", 'categoryPost');
+
+});
