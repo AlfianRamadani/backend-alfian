a8b34380b6003318b8b06e12b0eeb192c17ec7dc
Alfian Ramadani <Alfianr792@gmail.com>
2024-06-15 13:10:26 +0800

    feat: Add ProjectFactory for generating project data

---
+++ database/factories/ProjectFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\project>
+ */
+class ProjectFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => fake()->sentence(3),
+            'description' => fake()->paragraph(3),
+            'featured_img' => fake()->imageUrl(),
+        ];
+    }
+}
