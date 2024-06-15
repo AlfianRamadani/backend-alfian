af7eec2203214d1129cddba335af1e6079830822
Alfian Ramadani <Alfianr792@gmail.com>
2024-06-15 13:10:47 +0800

    refactor: Update information table migration to include satisfication field

--- database/migrations/2024_04_29_043845_information.php
+++ database/migrations/2024_04_29_043845_information.php
@@ -25,27 +25,14 @@ return new class extends Migration
             $table->string('contact_person');
             $table->string('projects_done');
             $table->string('experience');
-            $table->string('satisfication');
             $table->string('avatar')->nullable();
+            $table->string('satisfication');
             $table->timestamps();
         
         });
-        information::firstOrCreate([
-            "name" => "Alfian Ramadani",
-            "country" => "Indonesia",
-            "description_1" => "I am a FullStack Developer based in Indonesia, passionate about crafting robust and user-friendly web applications.",
-            "description_2"=> "I've been studying programming for 2 years, especially web programming.I am passionate about creating websites and am thrilled to present this portfolio, showcasing my skills and projects.",
-            "description_3" => "My journey in web development is driven by a strong motivation to learn and grow. I approach each project with enthusiasm and a positive attitude, always striving to create the best user experience possible.",
-            "description_4" => "As a FullStack Developer based in Indonesia, I specialize in both front-end and back-end web development. My journey in programming has been driven by a love for coding and problem-solving, and I am always eager to learn new technologies and improve my craft. This portfolio is a testament to my dedication and the diverse projects I have worked on.",
-            "email" => "Alfianr792@gmail.com",
-            "position" => "FullStack Developer",
-            "contact_person" => "+62 895-3418-13016",
-            'projects_done' => "10",
-            "experience" => "2",
-            "satisfication" => "98"
-
-        ]);
-    }
+       
+      
+   }
 
     /**
      * Reverse the migrations.
