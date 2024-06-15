e6f1c6813f58c906ac64ebf9db8a615687574440
Alfian Ramadani <Alfianr792@gmail.com>
2024-06-15 13:10:11 +0800

    refactor: Update app.php to handle redirecting guests to the login page

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,6 +4,8 @@ use App\Helpers\ResponseFormatter;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Exception\MethodNotAllowedException;
 
@@ -17,17 +19,19 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->redirectGuestsTo('/login-page');
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        $exceptions->render(function (NotFoundHttpException $e) {
-            return response()->json([
-                'message' => 'Not Foundd',
-                'status' => 404
+        $exceptions->render(function (NotFoundHttpException $e, Request $request) {
+            if ($request->is('api/*')) {
+                return response()->json([
+                    'message' => 'Not Foundd',
+                    'status' => 404
 
-            ]);
+                ]);
+            }
+            
         });
-        $exceptions->render(function (MethodNotAllowedException $e) {
-            return ResponseFormatter::error(NULL, "Method Not Allowed", 405);
-        });
+
+    
     })->create();
