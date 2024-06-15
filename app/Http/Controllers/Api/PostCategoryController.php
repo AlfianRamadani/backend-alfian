1969729183f4a6271ac243d19fc8f995a09123cb
Alfian Ramadani <Alfianr792@gmail.com>
2024-06-15 13:06:43 +0800

    feat: Add PostCategoryController for managing post category data

---
+++ app/Http/Controllers/Api/PostCategoryController.php
@@ -0,0 +1,68 @@
+<?php
+
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\post_category;
+use App\Http\Requests\Storepost_categoryRequest;
+use App\Http\Requests\Updatepost_categoryRequest;
+
+class PostCategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store()
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show()
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit()
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update( )
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy()
+    {
+        //
+    }
+}
