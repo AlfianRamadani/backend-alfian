745dfd69e55625059b92334cd68430403e003685
Alfian Ramadani <Alfianr792@gmail.com>
2024-06-15 13:11:05 +0800

    refactor: Rename 'journeys' table to 'experiences' and remove unnecessary code

--- database/migrations/2024_05_09_141653_create_journeys_table.php
+++ database/migrations/2024_05_09_141653_create_journeys_table.php
@@ -12,41 +12,15 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('journeys', function (Blueprint $table) {
+        Schema::create('experiences', function (Blueprint $table) {
             $table->id();
             $table->string('work');
             $table->string('division');
             $table->string('period');
-            $table->foreignId("information_id")->constrained("informations")->cascadeOnDelete()->cascadeOnUpdate();
 
             $table->timestamps();
         });
-        $journeys = [
-            [
-                "work" => "Google Inc",
-                "division" => "Software Engineering",
-                "period" => "2030-2035",
-                "information_id" => 1
-
-            ],
-            [
-                "work" => "Twitter Inc.",
-                "division" => "Visual Creator",
-                "period" => "2035-2039",
-                "information_id" => 1
-
-            ],
-            [
-                "work" => "Facebook Inc.",
-                "division" => "Software Engineering",
-                "period" => "2040-2045",
-                "information_id" => 1
-
-            ]
-        ];
-        collect($journeys)->each(function ($journey) {
-            Journey::create($journey);
-        }); 
+        
         
     }
 
