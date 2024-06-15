1c50bb3713e9cbbfa8205931981a67a487d6b8bb
Alfian Ramadani <Alfianr792@gmail.com>
2024-06-15 13:10:59 +0800

    refactor: Remove unnecessary code from create_educations migration

--- database/migrations/2024_05_09_141639_create_educations.php
+++ database/migrations/2024_05_09_141639_create_educations.php
@@ -18,34 +18,9 @@ return new class extends Migration
             $table->string('school');
             $table->string('major');
             $table->string('period');
-            $table->foreignId("information_id")->constrained("informations")->cascadeOnDelete()->cascadeOnUpdate();
             $table->timestamps();
         });
-        $educations = [
-            [
-                "school" => "MTsN 1 Balikpapan",
-                "major" => "Islamic School",
-                "period" => "2019-2022",
-                "information_id" => 1
-
-            ],
-            [
-                "school" => "SMK Negeri 2 Balikpapan",
-                "major" => "Rekayasa Perangkat Lunak",
-                "period" => "2022-2025",
-                "information_id" => 1
-
-            ],
-            [
-                "school" => "Institut Teknologi Bandung",
-                "major" => "Teknik Informatika",
-                "period" => "2025-2029",
-                "information_id" => 1
-
-            ]
-        ];
-        collect($educations)->each(function ($education) {
-            Education::create($education);}); 
+     
     }
 
     /**
